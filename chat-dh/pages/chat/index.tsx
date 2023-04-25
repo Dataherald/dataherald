04ae5cfd773b390c182581503edaa00b067fdbf2
Juan Valacco <97040903+jvalacco-dataherald@users.noreply.github.com>
2023-04-24 21:13:11 -0300

    delete commented code

--- chat-dh/pages/chat/index.tsx
+++ chat-dh/pages/chat/index.tsx
@@ -54,36 +54,6 @@ export default function Home() {
       },
     ]);
 
-    // const reader = data.getReader();
-    // const decoder = new TextDecoder();
-    // let done = false;
-    // let isFirst = true;
-
-    // while (!done) {
-    //   const { value, done: doneReading } = await reader.read();
-    //   done = doneReading;
-    //   const chunkValue = decoder.decode(value);
-
-    //   if (isFirst) {
-    //     isFirst = false;
-    //     setMessages((messages) => [
-    //       ...messages,
-    //       {
-    //         role: "assistant",
-    //         content: chunkValue,
-    //       },
-    //     ]);
-    //   } else {
-    //     setMessages((messages) => {
-    //       const lastMessage = messages[messages.length - 1];
-    //       const updatedMessage = {
-    //         ...lastMessage,
-    //         content: lastMessage.content + chunkValue,
-    //       };
-    //       return [...messages.slice(0, -1), updatedMessage];
-    //     });
-    //   }
-    // }
   };
 
   const handleExample = (prompt: string) => {
