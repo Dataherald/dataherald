5cbda7d6c0f01d8fe97829746dda7ad89fbe9766
Juan Valacco <97040903+jvalacco-dataherald@users.noreply.github.com>
2023-05-03 11:06:21 -0300

    DH-3926 add max character validation of 1k max

--- chat-dh/components/Chat/ChatInput.tsx
+++ chat-dh/components/Chat/ChatInput.tsx
@@ -1,5 +1,5 @@
 import { Message } from "@/types/chat";
-import { FC, KeyboardEvent, useLayoutEffect, useRef, useState } from "react";
+import { FC, KeyboardEvent, useRef, useState } from "react";
 import { Icon } from "../Layout/Icon";
 
 interface ChatInputProps {
@@ -7,19 +7,25 @@ interface ChatInputProps {
   onSend: (message: Message) => void;
 }
 
+const VALIDATIONS = {
+  MAX_CHARACTERS: 1000,
+};
+
 export const ChatInput: FC<ChatInputProps> = ({ disabled = false, onSend }) => {
-  const [content, setContent] = useState<string>();
+  const [content, setContent] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
 
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const value = e.target.value;
-    if (value.length > 4000) {
-      alert("Message limit is 4000 characters");
-      return;
-    }
-
     setContent(value);
+
+    if (value.length > VALIDATIONS.MAX_CHARACTERS) {
+      setError(`Message limit is ${VALIDATIONS.MAX_CHARACTERS} characters`);
+    } else {
+      setError(null);
+    }
   };
 
   const handleSend = () => {
@@ -33,16 +39,9 @@ export const ChatInput: FC<ChatInputProps> = ({ disabled = false, onSend }) => {
   const handleKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.key === "Enter" && !e.shiftKey) {
       e.preventDefault();
-      handleSend();
+      if (!error) handleSend();
     }
   };
-
-  useLayoutEffect(() => {
-    if (textareaRef && textareaRef.current) {
-      textareaRef.current.style.height = "inherit";
-      textareaRef.current.style.minHeight = `${textareaRef.current?.scrollHeight}px`;
-    }
-  }, [content]);
 
   return (
     <div className="relative">
@@ -50,7 +49,7 @@ export const ChatInput: FC<ChatInputProps> = ({ disabled = false, onSend }) => {
         ref={textareaRef}
         className={`min-h-[44px] rounded-lg pl-4 pr-12 py-2 w-full focus:outline-none focus:ring-1 focus:ring-neutral-300 border border-black border-opacity-80 ${
           disabled && "opacity-80"
-        }`}
+        } ${error && "border-yellow-600 text-yellow-600"}`}
         style={{ resize: "none" }}
         placeholder={
           disabled
@@ -63,15 +62,20 @@ export const ChatInput: FC<ChatInputProps> = ({ disabled = false, onSend }) => {
         onKeyDown={handleKeyDown}
         disabled={disabled}
       />
-      <button onClick={() => handleSend()} disabled={disabled}>
+      <button onClick={() => handleSend()} disabled={disabled || !!error}>
         <Icon
           value="send"
           type="filled"
-          className={`text-secondary-dark absolute right-2 bottom-3 h-8 w-8 rounded-full p-1 ${
-            disabled ? "opacity-50" : "hover:opacity-80 hover:cursor-pointer"
-          }`}
+          className={`text-secondary-dark absolute right-2 h-8 w-8 rounded-full p-1 ${
+            disabled || !!error
+              ? "opacity-50"
+              : "hover:opacity-80 hover:cursor-pointer"
+          } ${error ? "bottom-[4rem]" : "bottom-[2.3rem]"}`}
         />
       </button>
+      {error && (
+        <div className="text-yellow-600 mt-2 pl-4 text-sm">{error}</div>
+      )}
     </div>
   );
 };
