17a8ab748306cc78954be6e1a12c1939e5c32d9a
Juan Carlos Jose Camacho <juan.jose@oyster.io>
2023-07-05 14:10:55 -0600

    DH-4216 Move .env file at root and fix initdb file for mongo

---
+++ initdb.d/init-mongo.sh
@@ -0,0 +1,14 @@
+set -e
+
+mongosh <<EOF
+use $MONGO_INITDB_DATABASE
+
+db.createUser({
+  user: '$MONGO_INITDB_ROOT_USERNAME',
+  pwd: '$MONGO_INITDB_ROOT_PASSWORD',
+  roles: [{
+    role: 'readWrite',
+    db: '$MONGO_INITDB_DATABASE'
+  }]
+})
+EOF
