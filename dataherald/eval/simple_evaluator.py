a4c50552748b45713bba852c2408879435724a4a
Juan Carlos José Camacho <jcjc712@hotmail.com>
2023-07-06 15:34:31 -0600

    DH-4207 Fix to NLQueryResponse model (#21)
    Co-authored-by: Juan Carlos Jose Camacho <jc@dataherald.com>

--- dataherald/eval/simple_evaluator.py
+++ dataherald/eval/simple_evaluator.py
@@ -5,12 +5,11 @@ from dataherald.eval import Evaluation, Evaluator
 
 class SimpleEvaluator(Evaluator):
     @override
-    def get_golden_sql(self, question: str) -> bool:
+    def get_golden_sql(self, question: str) -> str:  # noqa: ARG002
         """Obtains the golden SQL for the given question"""
-        pass
+        return "golden sql"
 
     @override
-    def evaluate(self, question: str, golden_sql: str) -> Evaluation:
+    def evaluate(self, question: str, golden_sql: str) -> Evaluation:  # noqa: ARG002
         """Evaluates a question with an SQL pair."""
-        print(question, golden_sql)
-        return Evaluation(id="", question_id="", score=0.5)
+        return Evaluation(question_id="", score=0.5)
