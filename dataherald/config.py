070104e913545bd30c5d2af5c0437da19a3a77f5
Amir A. Zohrenejad <aazo11@gmail.com>
2023-07-10 09:05:14 -0700

    add changes to config.py

--- dataherald/config.py
+++ dataherald/config.py
@@ -14,7 +14,8 @@ _abstract_type_keys: Dict[str, str] = {
     "dataherald.sql_generator.SQLGenerator": "sql_generator_impl",
     "dataherald.eval.Evaluator": "eval_impl",
     "dataherald.db.DB": "db_impl",
-    "dataherald.context_store.ContextStore": "context_store_impl"
+    "dataherald.context_store.ContextStore": "context_store_impl",
+    "dataherald.vector_store.VectorStore": "vector_store_impl"
 }
 
 
@@ -34,7 +35,8 @@ class Settings(BaseSettings):
     )
     db_impl: str = os.environ.get("DB", "dataherald.db.mongo.MongoDB")
 
-    context_store_impl: str = os.environ.get("CONTEXT_STORE", "dataherald.context_store.default.DefaultContextStore") 
+    context_store_impl: str = os.environ.get("CONTEXT_STORE", "dataherald.context_store.default.DefaultContextStore")
+    vector_store_impl: str = os.environ.get("VECTOR_STORE", "dataherald.vector_store.chroma.Chroma")
     server_host: str | None = os.environ.get("SERVER_HOST")
     server_http_port: str | None = os.environ.get("SERVER_HTTP_PORT")
     server_ssl_enabled: bool | None = os.environ.get("SERVER_SSL_ENABLED")
