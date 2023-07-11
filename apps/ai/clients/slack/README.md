0dc8f5f7e6663a14d0eeca189f4e3e5d36f580cc
Juan Valacco <97040903+jvalacco-dataherald@users.noreply.github.com>
2023-07-11 17:32:36 -0300

    DH-4210 [ai] create slackbot client

---
+++ apps/ai/clients/slack/README.md
@@ -0,0 +1,18 @@
+# Slack Bot Client
+
+This is a Slack bot that listens for messages on companies workspaces and interact with our Dataherald AI API
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+-   Node.js
+-   pnpm
+
+### Installing
+
+1. Install dependencies: `pnpm install`
+2. Copy `.env.example` variables to `.env` and fill them up
+3. Start the server: `pnpm dev`
---
+++ apps/ai/clients/slack/server/index.js
@@ -0,0 +1,80 @@
+const { App } = require('@slack/bolt')
+const { log } = require('console')
+require('dotenv').config()
+
+const app = new App({
+    token: process.env.SLACK_BOT_ACCESS_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    socketMode: true,
+    appToken: process.env.SLACK_APP_TOKEN,
+})
+
+const API_URL = process.env.DH_AI_API_URL
+
+// Listens to incoming messages in direct messages with the bot
+app.message(async ({ message, say }) => {
+    const { user, text: userMessage } = message
+    await say({
+        blocks: [
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text: `:wave: Hello, <@${user}>. I will look up in your database for an answer to your inquiry. Please, give me a few moments and I'll get back to you.`,
+                },
+            },
+        ],
+        text: 'Fallback text for notifications',
+    })
+
+    try {
+        const query = encodeURIComponent(userMessage)
+        const endpointUrl = `${API_URL}/question?question=${query}`
+        log('fetching data from', endpointUrl)
+        const response = await fetch(endpointUrl, {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+        })
+        const data = await response.json()
+        const { nl_response, sql_query, exec_time } = data
+        const execTime = parseFloat(exec_time).toFixed(2)
+
+        await say({
+            blocks: [
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: `:mag: *Response*: ${nl_response}`,
+                    },
+                },
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: `:memo: *Generated SQL Query*: \n \`\`\`${sql_query}\`\`\``,
+                    },
+                },
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: `:stopwatch: *Execution Time*: ${execTime}s`,
+                    },
+                },
+            ],
+            text: 'Fallback text for notifications',
+        })
+    } catch (e) {
+        log('Something went wrong: ', e)
+    }
+})
+
+async function startServer() {
+    const appPort = process.env.PORT || 3000
+    await app.start(appPort)
+
+    log(`Dataherald AI Slack client is running on http//:localhost:${appPort}`)
+}
+
+startServer()
