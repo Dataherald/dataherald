648930e0893bf6d9889195b38bb2b266c63026cf
Juan Valacco <97040903+jvalacco-dataherald@users.noreply.github.com>
2023-07-13 18:45:17 -0300

    DH-4245 [ai] remove old chat location

--- chat-dh/pages/api/auth/token/index.ts
+++
@@ -1,9 +0,0 @@
-import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default withApiAuthRequired(
-  async (req: NextApiRequest, res: NextApiResponse) => {
-    const { accessToken } = await getAccessToken(req, res);
-    res.status(200).json(accessToken);
-  },
-);
