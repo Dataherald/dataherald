c7c6b0435c95da13bd696ae6691b165bbbfca483
mohammadrezapourreza <m1378.prz@gmail.com>
2023-07-18 11:29:03 -0400

    issue

--- dataherald/tests/evaluator/test_eval.py
+++ dataherald/tests/evaluator/test_eval.py
@@ -6,5 +6,7 @@ from dataherald.types import NLQuery, NLQueryResponse
 
 class TestEvaluator(Evaluator):
     @overrides
-    def evaluate(self, question: NLQuery, generated_answer: NLQueryResponse) -> Evaluation:
+    def evaluate(
+        self, question: NLQuery, generated_answer: NLQueryResponse
+    ) -> Evaluation:
         return super().evaluate(question, generated_answer)
