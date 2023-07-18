3716c5a04721231a51d5082e787c5282ed9d0883
mohammadrezapourreza <m1378.prz@gmail.com>
2023-07-18 10:51:04 -0400

    added abstract class extend

--- dataherald/tests/evaluator/test_eval.py
+++ dataherald/tests/evaluator/test_eval.py
@@ -1,9 +1,17 @@
 from dataherald.config import System
+from dataherald.eval import Evaluation, Evaluator
+from dataherald.types import NLQuery, NLQueryResponse
 
 
-class TestEvaluator:
+class TestEvaluator(Evaluator):
     def __init__(self, system: System):
         pass
 
     def start(self):
         pass
+
+    def is_acceptable_response(self, question: NLQuery, generated_answer: NLQueryResponse) -> bool:
+        return super().is_acceptable_response(question, generated_answer)
+
+    def evaluate(self, question: NLQuery, generated_answer: NLQueryResponse) -> Evaluation:
+        return super().evaluate(question, generated_answer)
