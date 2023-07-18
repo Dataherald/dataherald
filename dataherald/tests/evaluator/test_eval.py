c326a4388bf52858feb07adb952c2dfec0660f3f
mohammadrezapourreza <m1378.prz@gmail.com>
2023-07-18 11:15:57 -0400

    issue

--- dataherald/tests/evaluator/test_eval.py
+++ dataherald/tests/evaluator/test_eval.py
@@ -1,24 +1,5 @@
-from typing import override
-
-from dataherald.config import System
-from dataherald.eval import Evaluation, Evaluator
-from dataherald.types import NLQuery, NLQueryResponse
+from dataherald.eval import Evaluator
 
 
 class TestEvaluator(Evaluator):
-    def __init__(self, system: System):
-        pass
-
-    def start(self):
-        pass
-
-    def is_acceptable_response(
-        self, question: NLQuery, generated_answer: NLQueryResponse
-    ) -> bool:
-        pass
-
-    @override
-    def evaluate(
-        self, question: NLQuery, generated_answer: NLQueryResponse
-    ) -> Evaluation:
-        pass
+    pass
