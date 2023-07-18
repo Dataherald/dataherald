cc3b875d49df9245d3aa21483fca6ff669bba09b
mohammadrezapourreza <m1378.prz@gmail.com>
2023-07-18 10:53:05 -0400

    black eval

--- dataherald/tests/evaluator/test_eval.py
+++ dataherald/tests/evaluator/test_eval.py
@@ -10,8 +10,12 @@ class TestEvaluator(Evaluator):
     def start(self):
         pass
 
-    def is_acceptable_response(self, question: NLQuery, generated_answer: NLQueryResponse) -> bool:
+    def is_acceptable_response(
+        self, question: NLQuery, generated_answer: NLQueryResponse
+    ) -> bool:
         return super().is_acceptable_response(question, generated_answer)
 
-    def evaluate(self, question: NLQuery, generated_answer: NLQueryResponse) -> Evaluation:
+    def evaluate(
+        self, question: NLQuery, generated_answer: NLQueryResponse
+    ) -> Evaluation:
         return super().evaluate(question, generated_answer)
