69275705b68bcddc82c72d8e4c97b2a3c2a2027f
mohammadrezapourreza <m1378.prz@gmail.com>
2023-07-18 11:34:06 -0400

    Hope to be finall issue

--- dataherald/tests/evaluator/test_eval.py
+++ dataherald/tests/evaluator/test_eval.py
@@ -1,12 +1,21 @@
 from overrides import overrides
 
+from dataherald.config import System
 from dataherald.eval import Evaluation, Evaluator
 from dataherald.types import NLQuery, NLQueryResponse
 
 
 class TestEvaluator(Evaluator):
+    def __init__(self, system: System):
+        pass
+
+    def is_acceptable_response(
+        self, question: NLQuery, generated_answer: NLQueryResponse
+    ) -> bool:
+        return True
+
     @overrides
     def evaluate(
         self, question: NLQuery, generated_answer: NLQueryResponse
     ) -> Evaluation:
-        return super().evaluate(question, generated_answer)
+        return Evaluation(question_id="0", answer_id="0", score=0.8)
