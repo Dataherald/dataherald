fae2f4a9b6b891c28fe0bdc915c139c029daba19
Juan Carlos Jose Camacho <jc@dataherald.com>
2023-07-25 15:40:35 -0600

    DH-4307 Test scanner endpoint

--- dataherald/tests/test_api.py
+++ dataherald/tests/test_api.py
@@ -5,11 +5,22 @@ from dataherald.app import app
 client = TestClient(app)
 
 HTTP_200_CODE = 200
+HTTP_404_CODE = 404
 
 
 def test_heartbeat():
     response = client.get("/api/v1/heartbeat")
     assert response.status_code == HTTP_200_CODE
+
+
+def test_scan_all_tables():
+    response = client.post("/api/v1/scanner?db_alias=foo")
+    assert response.status_code == HTTP_200_CODE
+
+
+def test_scan_one_table():
+    response = client.post("/api/v1/scanner?db_alias=foo&table_name=foo")
+    assert response.status_code == HTTP_404_CODE
 
 
 def test_answer_question():
