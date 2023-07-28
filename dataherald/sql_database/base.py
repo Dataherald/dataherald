9c35766aa54a1b801d1845052659c98186bb93db
Juan Carlos Jose Camacho <jc@dataherald.com>
2023-07-28 15:38:50 -0600

    DH-4324 Only creates one db instance

--- dataherald/sql_database/base.py
+++ dataherald/sql_database/base.py
@@ -13,7 +13,16 @@ from dataherald.utils.encrypt import FernetEncrypt
 logger = logging.getLogger(__name__)
 
 
-class SQLDatabase(LangchainSQLDatabase):
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(type(cls), cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class SQLDatabase(LangchainSQLDatabase, metaclass=Singleton):
     """SQL Database.
 
     Wrapper around SQLDatabase object from langchain. Offers
@@ -42,7 +51,8 @@ class SQLDatabase(LangchainSQLDatabase):
     ) -> "SQLDatabase":
         """Construct a SQLAlchemy engine from URI."""
         _engine_args = engine_args or {}
-        return cls(create_engine(database_uri, **_engine_args), **kwargs)
+        engine = create_engine(database_uri, **_engine_args)
+        return cls(engine, **kwargs)
 
     @classmethod
     def get_sql_engine(cls, database_info: DatabaseConnection) -> "SQLDatabase":
