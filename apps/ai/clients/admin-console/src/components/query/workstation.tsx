3185e3a0cef13e4c1642efa9b0b0a06ef7084dfd
Juan Valacco <97040903+jvalacco-dataherald@users.noreply.github.com>
2023-08-01 19:56:17 -0300

    DH-4278 [ai][admin-console] add verify swtich

--- apps/ai/clients/admin-console/src/components/query/workstation.tsx
+++ apps/ai/clients/admin-console/src/components/query/workstation.tsx
@@ -1,6 +1,7 @@
 import SqlEditor from '@/components/query/sql-editor'
 import { Button } from '@/components/ui/button'
-import { Query } from '@/models/api'
+import { Switch } from '@/components/ui/switch'
+import { EQueryStatus, Query } from '@/models/api'
 import { format } from 'date-fns'
 import {
   Calendar,
@@ -26,6 +27,7 @@ const QueryWorkstation: FC<QueryWorkstationProps> = ({ query }) => {
     nl_response,
     user: { name: userName },
     sql_query,
+    status,
     ai_process,
     last_updated,
   } = query
@@ -33,27 +35,46 @@ const QueryWorkstation: FC<QueryWorkstationProps> = ({ query }) => {
   const questionDate: Date = new Date(question_date)
   const lastUpdatedDate: Date = new Date(last_updated)
   const [currentSqlQuery, setCurrentSqlQuery] = useState(sql_query)
+  const [verified, setVerified] = useState(status === EQueryStatus.VERIFIED)
 
   const handleSqlChange = (value: string) => {
     setCurrentSqlQuery(value)
   }
 
+  const handleVerifyChange = (verified: boolean) => {
+    setVerified(verified)
+  }
+
   return (
     <div className="container p-0 flex-1 flex flex-col gap-5">
-      <div className="container p-0">
-        <h1 className="mb-1 font-bold">{question}</h1>
-        <h3 className="flex gap-5">
-          {[
-            { icon: User2, text: userName },
-            { icon: Calendar, text: format(questionDate, 'MMMM dd, yyyy') },
-            { icon: Clock, text: format(questionDate, 'hh:mm a') },
-          ].map((item, index) => (
-            <div key={index} className="flex items-center gap-1">
-              <item.icon size={16} />
-              <span>{item.text}</span>
-            </div>
-          ))}
-        </h3>
+      <div className="container p-0 flex justify-between gap-3">
+        <div className="container p-0">
+          <h1 className="mb-1 font-bold">{question}</h1>
+          <h3 className="flex gap-5">
+            {[
+              { icon: User2, text: userName },
+              { icon: Calendar, text: format(questionDate, 'MMMM dd, yyyy') },
+              { icon: Clock, text: format(questionDate, 'hh:mm a') },
+            ].map((item, index) => (
+              <div key={index} className="flex items-center gap-1">
+                <item.icon size={16} />
+                <span>{item.text}</span>
+              </div>
+            ))}
+          </h3>
+        </div>
+        <div id="verify" className="min-w-fit flex items-center gap-5 px-8">
+          {verified ? (
+            <span className="text-lg text-green-700">Verified</span>
+          ) : (
+            <span className="text-lg text-red-600">Mark as verified</span>
+          )}
+          <Switch
+            className="scale-125 data-[state=checked]:bg-green-700 data-[state=unchecked]:bg-red-600"
+            defaultChecked={verified}
+            onCheckedChange={handleVerifyChange}
+          />
+        </div>
       </div>
       <div className="bg-white border rounded-xl flex-1 flex flex-col gap-5 max-h-[40vh] p-8">
         <Tabs defaultValue="sql" className="w-full h-full">
