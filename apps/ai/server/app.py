c9e7e20ba829c7e0dcbe8fa4389bbe7767718109
Dishen <44216194+DishenWang2023@users.noreply.github.com>
2023-08-03 19:15:37 -0500

    DH-4330 add cors middleware (#53)

--- apps/ai/server/app.py
+++ apps/ai/server/app.py
@@ -1,11 +1,21 @@
 import time
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from modules.k2_core import controller as k2_controller
 from modules.query import controller as query_controller
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(k2_controller.router)
 app.include_router(query_controller.router)
