1fdbf7538bb7504e12a27c4532fa7782e05c78b3
Juan Carlos Jose Camacho <jc@dataherald.com>
2023-08-07 17:11:05 -0600

    DH-4380 Fix format response

--- dataherald/sql_generator/__init__.py
+++ dataherald/sql_generator/__init__.py
@@ -10,6 +10,7 @@ from langchain.chat_models import ChatOpenAI
 from dataherald.config import Component, System
 from dataherald.sql_database.base import SQLDatabase
 from dataherald.sql_database.models.types import DatabaseConnection
+from dataherald.sql_generator.create_sql_query_status import create_sql_query_status
 from dataherald.types import NLQuery, NLQueryResponse, SQLQueryResult
 
 
@@ -29,43 +30,7 @@ class SQLGenerator(Component, ABC):
     def create_sql_query_status(
         self, db: SQLDatabase, query: str, response: NLQueryResponse
     ) -> NLQueryResponse:
-        """Find the sql query status and populate the fields sql_query_result, sql_generation_status, and error_message"""
-        if query == "":
-            response.sql_generation_status = "NONE"
-            response.sql_query_result = None
-            response.error_message = None
-        else:
-            try:
-                execution = db.engine.execute(query)
-                columns = execution.keys()
-                result = execution.fetchall()
-                if len(result) == 0:
-                    response.sql_query_result = None
-                else:
-                    columns = [item for item in columns]  # noqa: C416
-                    rows = []
-                    for row in result:
-                        modified_row = {}
-                        for key, value in zip(row.keys(), row, strict=True):
-                            if (
-                                type(value) is date
-                            ):  # Check if the value is an instance of datetime.date
-                                modified_row[key] = datetime(
-                                    value.year, value.month, value.day
-                                )
-                            else:
-                                modified_row[key] = value
-                        rows.append(modified_row)
-                    response.sql_query_result = SQLQueryResult(
-                        columns=columns, rows=rows
-                    )
-                response.sql_generation_status = "VALID"
-                response.error_message = None
-            except Exception as e:
-                response.sql_generation_status = "INVALID"
-                response.sql_query_result = None
-                response.error_message = str(e)
-        return response
+        return create_sql_query_status(db, query, response)
 
     @abstractmethod
     def generate_response(
--- dataherald/sql_generator/generates_nl_answer.py
+++ dataherald/sql_generator/generates_nl_answer.py
@@ -1,58 +1,15 @@
-from datetime import date, datetime
-
 import openai
 
 from dataherald.repositories.nl_question import NLQuestionRepository
 from dataherald.sql_database.base import SQLDatabase
 from dataherald.sql_database.models.types import DatabaseConnection
-from dataherald.types import NLQueryResponse, SQLQueryResult
+from dataherald.sql_generator.create_sql_query_status import create_sql_query_status
+from dataherald.types import NLQueryResponse
 
 
 class GeneratesNlAnswer:
     def __init__(self, storage):
         self.storage = storage
-
-    def create_sql_query_status(
-        self, db: SQLDatabase, query: str, response: NLQueryResponse
-    ) -> NLQueryResponse:
-        """Find the sql query status and populate the fields sql_query_result, sql_generation_status, and error_message"""
-        response.sql_query = query
-        if query == "":
-            response.sql_generation_status = "NONE"
-            response.sql_query_result = None
-            response.error_message = None
-        else:
-            try:
-                execution = db.engine.execute(query)
-                columns = execution.keys()
-                result = execution.fetchall()
-                if len(result) == 0:
-                    response.sql_query_result = None
-                else:
-                    columns = [item for item in columns]  # noqa: C416
-                    rows = []
-                    for row in result:
-                        modified_row = {}
-                        for key, value in zip(row.keys(), row, strict=True):
-                            if (
-                                type(value) is date
-                            ):  # Check if the value is an instance of datetime.date
-                                modified_row[key] = datetime(
-                                    value.year, value.month, value.day
-                                )
-                            else:
-                                modified_row[key] = value
-                        rows.append(modified_row)
-                    response.sql_query_result = SQLQueryResult(
-                        columns=columns, rows=rows
-                    )
-                response.sql_generation_status = "VALID"
-                response.error_message = None
-            except Exception as e:
-                response.sql_generation_status = "INVALID"
-                response.sql_query_result = None
-                response.error_message = str(e)
-        return response
 
     def execute(self, nl_query_response: NLQueryResponse) -> NLQueryResponse:
         nl_question_repository = NLQuestionRepository(self.storage)
@@ -65,7 +22,7 @@ class GeneratesNlAnswer:
         )
         database_connection = DatabaseConnection(**db_connection)
         database = SQLDatabase.get_sql_engine(database_connection)
-        nl_query_response = self.create_sql_query_status(
+        nl_query_response = create_sql_query_status(
             database, nl_query_response.sql_query, nl_query_response
         )
         chat_completion = openai.ChatCompletion.create(
