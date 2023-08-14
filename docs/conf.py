ca8e22d0ff98120832e98b9a7d5156842c711191
mohammadrezapourreza <m1378.prz@gmail.com>
2023-08-14 17:54:28 -0400

    DH-4419/reformat with black

--- docs/conf.py
+++ docs/conf.py
@@ -11,23 +11,22 @@ import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 
-project = 'Dataheral'
-copyright = '2023, Dataherald'
-author = 'Dataherald'
-release = '0.0.1'
+project = "Dataheral"
+copyright = "2023, Dataherald"
+author = "Dataherald"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
