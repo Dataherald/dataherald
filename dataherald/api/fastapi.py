9a023eae63bb3bb06b500a3a80e0c0119b450610
mohammadrezapourreza <m1378.prz@gmail.com>
2023-08-17 09:57:01 -0400

    DH-4459/reformat with black

--- dataherald/api/fastapi.py
+++ dataherald/api/fastapi.py
@@ -202,13 +202,15 @@ class FastAPI(API):
         else:
             evaluator = self.system.instance(Evaluator)
             db_connection = self.storage.find_one(
-            "database_connection", {"alias": nl_question.db_alias}
+                "database_connection", {"alias": nl_question.db_alias}
             )
             if not db_connection:
-                raise HTTPException(status_code=404, detail="Database connection not found")
+                raise HTTPException(
+                    status_code=404, detail="Database connection not found"
+                )
             database_connection = DatabaseConnection(**db_connection)
             confidence_score = evaluator.get_confidence_score(
-            nl_question, nl_query_response, database_connection
+                nl_question, nl_query_response, database_connection
             )
             nl_query_response.confidence_score = confidence_score
             question_id = str(nl_query_response.nl_question_id)
