70e9c4dc700e62a889ad8e37b829f921b57f217a
Paul Markus <pmarkus@rockhampton-mgt.com>
2023-09-05 16:00:40 +0900

    added to the scanner config

--- scripts/DB_SETUP.bat
+++ scripts/DB_SETUP.bat
@@ -1,7 +1,11 @@
 # run all the setup python scripts in the current directory in the following order
+# 0. remove all the existing tables in the database
 # 1. setup_database.py
 # 2. setup_scanner.py
 # 3. setup_golden_records.py
+
+# delete all the data under the dbdata folder
+del /Q dbdata\*
 
 # 1. setup_database.py
 python setup_database.py
