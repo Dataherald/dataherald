7e25c7cc10a8da0b586129cfe599049b56fc5097
Paul Markus <pmarkus@rockhampton-mgt.com>
2023-09-07 14:24:27 +0900

    updated configs

--- scripts/DB_SETUP.bat
+++ scripts/DB_SETUP.bat
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 @REM # run all the setup python scripts in the current directory in the following order
 @REM # 0. remove all the existing tables in the database
 @REM # 1. setup_database.py
@@ -21,4 +22,18 @@ python setup_scanner.py
 
 @REM # 3. setup_golden_records.py
 echo "Setting up the golden records"
+=======
+# run all the setup python scripts in the current directory in the following order
+# 1. setup_database.py
+# 2. setup_scanner.py
+# 3. setup_golden_records.py
+
+# 1. setup_database.py
+python setup_database.py
+
+# 2. setup_scanner.py
+python setup_scanner.py
+
+# 3. setup_golden_records.py
+>>>>>>> 5937308 (added set up scripts)
 python setup_golden_records.py
