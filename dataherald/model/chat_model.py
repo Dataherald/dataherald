5ac8944d147f26e5dbac181a75e1b0a9f645b8f3
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-09-09 11:42:32 -0700

    v0 litellm

--- dataherald/model/chat_model.py
+++ dataherald/model/chat_model.py
@@ -1,7 +1,7 @@
 import os
 from typing import Any
 
-from langchain.chat_models import ChatAnthropic, ChatGooglePalm, ChatOpenAI
+from langchain.chat_models import ChatAnthropic, ChatGooglePalm, ChatOpenAI, ChatLiteLLM
 from overrides import override
 
 from dataherald.model import LLMModel
@@ -18,11 +18,11 @@ class ChatModel(LLMModel):
     @override
     def get_model(self, **kwargs: Any) -> Any:
         if self.openai_api_key:
-            self.model = ChatOpenAI(model_name=self.model_name, **kwargs)
+            self.model = ChatLiteLLM(model_name=self.model_name, **kwargs)
         elif self.anthropic_api_key:
-            self.model = ChatAnthropic(model=self.model, **kwargs)
+            self.model = ChatLiteLLM(model=self.model, **kwargs)
         elif self.google_api_key:
-            self.model = ChatGooglePalm(model_name=self.model_name, **kwargs)
+            self.model = ChatLiteLLM(model_name=self.model_name, **kwargs)
         else:
             raise ValueError("No valid API key environment variable found")
         return self.model
