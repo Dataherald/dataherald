a7fb3c7db1ba0e3d01680325d56783edcb151e20
mohammadrezapourreza <m1378.prz@gmail.com>
2023-09-11 13:19:49 -0400

    DH-4626/update code to work with LiteLLM

--- dataherald/model/chat_model.py
+++ dataherald/model/chat_model.py
@@ -1,7 +1,7 @@
 import os
 from typing import Any
 
-from langchain.chat_models import ChatAnthropic, ChatGooglePalm, ChatOpenAI
+from langchain.chat_models import ChatLiteLLM
 from overrides import override
 
 from dataherald.model import LLMModel
@@ -11,18 +11,18 @@ class ChatModel(LLMModel):
     def __init__(self, system):
         super().__init__(system)
         self.model_name = os.environ.get("LLM_MODEL", "gpt-4-32k")
-        self.openai_api_key = os.environ.get("OPENAI_API_KEY")
-        self.anthropic_api_key = os.environ.get("ANTHROPIC_API_KEY")
-        self.google_api_key = os.environ.get("GOOGLE_API_KEY")
+        self.api_keys = {
+            "openai_api_key": os.environ.get("OPENAI_API_KEY"),
+            "anthropic_api_key": os.environ.get("ANTHROPIC_API_KEY"),
+            "google_api_key": os.environ.get("GOOGLE_API_KEY"),
+            "cohere_api_key": os.environ.get("COHERE_API_KEY"),
+        }
 
     @override
     def get_model(self, **kwargs: Any) -> Any:
-        if self.openai_api_key:
-            self.model = ChatOpenAI(model_name=self.model_name, **kwargs)
-        elif self.anthropic_api_key:
-            self.model = ChatAnthropic(model=self.model, **kwargs)
-        elif self.google_api_key:
-            self.model = ChatGooglePalm(model_name=self.model_name, **kwargs)
-        else:
-            raise ValueError("No valid API key environment variable found")
-        return self.model
+        for _, api_key in self.api_keys.items():
+            if api_key:
+                self.model = ChatLiteLLM(model_name=self.model_name, **kwargs)
+                return self.model
+
+        raise ValueError("No valid API key environment variable found")
