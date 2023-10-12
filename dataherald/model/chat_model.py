dab16924566f7bc4b3b7ca9b0819422fead43ccc
Mohammadreza Pourreza <71866535+MohammadrezaPourreza@users.noreply.github.com>
2023-10-12 11:59:29 -0400

    Dh 4627/support litellm (#205)
    * DH-4627/upgrading langchain and use litellm
    
    * DH-4627/reformat with black

--- dataherald/model/chat_model.py
+++ dataherald/model/chat_model.py
@@ -1,7 +1,7 @@
 import os
 from typing import Any
 
-from langchain.chat_models import ChatAnthropic, ChatGooglePalm, ChatOpenAI
+from langchain.chat_models import ChatLiteLLM
 from overrides import override
 
 from dataherald.model import LLMModel
@@ -13,9 +13,6 @@ class ChatModel(LLMModel):
     def __init__(self, system):
         super().__init__(system)
         self.model_name = os.environ.get("LLM_MODEL", "gpt-4-32k")
-        self.openai_api_key = os.environ.get("OPENAI_API_KEY")
-        self.anthropic_api_key = os.environ.get("ANTHROPIC_API_KEY")
-        self.google_api_key = os.environ.get("GOOGLE_API_KEY")
 
     @override
     def get_model(
@@ -30,23 +27,14 @@ class ChatModel(LLMModel):
                 database_connection.llm_credentials.api_key
             )
             if model_family == "openai":
-                self.openai_api_key = api_key
+                os.environ["OPENAI_API_KEY"] = api_key
             elif model_family == "anthropic":
-                self.anthropic_api_key = api_key
+                os.environ["ANTHROPIC_API_KEY"] = api_key
             elif model_family == "google":
-                self.google_api_key = api_key
-        if self.openai_api_key:
-            self.model = ChatOpenAI(
-                model_name=self.model_name, openai_api_key=self.openai_api_key, **kwargs
-            )
-        elif self.anthropic_api_key:
-            self.model = ChatAnthropic(
-                model=self.model, anthropic_api_key=self.anthropic_api_key, **kwargs
-            )
-        elif self.google_api_key:
-            self.model = ChatGooglePalm(
-                model_name=self.model_name, google_api_key=self.google_api_key, **kwargs
-            )
-        else:
-            raise ValueError("No valid API key environment variable found")
-        return self.model
+                os.environ["GOOGLE_API_KEY"] = api_key
+            elif model_family == "cohere":
+                os.environ["COHERE_API_KEY"] = api_key
+        try:
+            return ChatLiteLLM(model_name=self.model_name, **kwargs)
+        except Exception as e:
+            raise ValueError("No valid API key environment variable found") from e
