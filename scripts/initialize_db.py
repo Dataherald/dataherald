f34cbf3cac2821901ace98bb8167c182747d725c
Paul Markus <pmarkus@rockhampton-mgt.com>
2023-10-17 13:49:48 +0900

    try this to make the write dbdata directory

--- scripts/initialize_db.py
+++ scripts/initialize_db.py
@@ -9,34 +9,34 @@ import shutil
 from MongoDB import MongoDB
 
 if __name__ == "__main__":
-    print("################################################################################")
-    print("                      Initialize Database Folder")
-    print("################################################################################")
-    # print the current working directory
-    print(f"Current working directory: {os.getcwd()}")
+  print("################################################################################")
+  print("                      Initialize Database Folder")
+  print("################################################################################")
+  # print the current working directory
+  print(f"Current working directory: {os.getcwd()}")
 
-    # delete the dbdata folder if it exists
-    if os.path.isdir("dbdata"):
-        print("Deleting existing dbdata folder")
-        shutil.rmtree("dbdata")
+  # delete the dbdata folder if it exists
+  if os.path.isdir("/app/dbdata"):
+    print("Deleting existing dbdata folder")
+    shutil.rmtree("/app/dbdata")
 
-    # create the dbdata folder
-    os.mkdir("dbdata")
-    print("Created new dbdata folder in current working directory")
-    print()
+  # create the dbdata folder
+  os.mkdir("/app/dbdata")
+  print("Created new dbdata folder in current working directory")
+  print()
 
-    print("################################################################################")
-    print("                      Initialize Database collections")
-    print("################################################################################")
-    # create a MongoDB object and drop the following collections
-    # database_connections
-    # golden_records
-    # table_descriptions
-    mongo = MongoDB()
-    mongo.drop_collection("database_connections")
-    mongo.drop_collection("golden_records")
-    mongo.drop_collection("table_descriptions")
-    mongo.drop_collection("instructions")
-    mongo.close()
+  print("################################################################################")
+  print("                      Initialize Database collections")
+  print("################################################################################")
+  # create a MongoDB object and drop the following collections
+  # database_connections
+  # golden_records
+  # table_descriptions
+  mongo = MongoDB()
+  mongo.drop_collection("database_connections")
+  mongo.drop_collection("golden_records")
+  mongo.drop_collection("table_descriptions")
+  mongo.drop_collection("instructions")
+  mongo.close()
 
-    print("################################################################################")
+  print("################################################################################")
