6d07cb83ec63e20395141e098ebecd1310f0d302
Mohammadreza Pourreza <71866535+MohammadrezaPourreza@users.noreply.github.com>
2023-11-06 10:00:43 -0500

    DH-4992/creating adaptive agent executor (#234)
    * DH-4992/creating adaptive agent executor
    
    * DH-4922/reducing the env vars and update the token counter
    
    * DH-4922/reformat with black

---
+++ dataherald/utils/models_context_window.py
@@ -0,0 +1,14 @@
+OPENAI_CONTEXT_WIDNOW_SIZES = {
+    "gpt-4": 8000,
+    "gpt-4-0613": 8000,
+    "gpt-4-32k": 32000,
+    "gpt-4-32k-0613": 32000,
+    "gpt-4-0314": 8000,
+    "gpt-4-32k-0314": 32000,
+    "gpt-3.5-turbo": 4000,
+    "gpt-3.5-turbo-16k": 16000,
+    "gpt-3.5-turbo-instruct": 4000,
+    "gpt-3.5-turbo-0613": 4000,
+    "gpt-3.5-turbo-16k-0613": 16000,
+    "gpt-3.5-turbo-0301": 4000,
+}
