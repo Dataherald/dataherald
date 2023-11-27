6232b8c2b6455ff5f68ef66e612108c89e687640
Juan Carlos José Camacho <jcjc712@hotmail.com>
2023-11-27 11:15:52 -0600

    [DH-5028] Improve flag to set ONLY_STORE_CSV_FILES_LOCALLY (#264)

--- dataherald/config.py
+++ dataherald/config.py
@@ -50,7 +50,9 @@ class Settings(BaseSettings):
     encrypt_key: str = os.environ.get("ENCRYPT_KEY")
     s3_aws_access_key_id: str | None = os.environ.get("S3_AWS_ACCESS_KEY_ID")
     s3_aws_secret_access_key: str | None = os.environ.get("S3_AWS_SECRET_ACCESS_KEY")
-    only_store_csv_files_locally: str | None = os.environ.get("ONLY_STORE_CSV_FILES_LOCALLY")
+    only_store_csv_files_locally: bool | None = os.environ.get(
+        "ONLY_STORE_CSV_FILES_LOCALLY", False
+    )
 
     def require(self, key: str) -> Any:
         val = self[key]
