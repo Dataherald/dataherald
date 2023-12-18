35598e70919c6d2a09e4b7bca9c93f0db43f63e0
Juan Carlos Jose Camacho <jc@dataherald.com>
2023-12-18 13:16:21 -0600

    Create Prompt service

--- dataherald/services/prompts.py
+++ dataherald/services/prompts.py
@@ -1,11 +1,30 @@
 from dataherald.api.types.requests import PromptRequest
+from dataherald.repositories.database_connections import (
+    DatabaseConnectionNotFoundError,
+    DatabaseConnectionRepository,
+)
 from dataherald.repositories.prompts import PromptRepository
 from dataherald.types import Prompt
 
 
 class PromptService:
     def __init__(self, storage):
-        self.prompt_repository = PromptRepository(storage)
+        self.storage = storage
+        self.prompt_repository = PromptRepository(self.storage)
 
     def create(self, prompt_request: PromptRequest) -> Prompt:
-        pass
+        db_connection_repository = DatabaseConnectionRepository(self.storage)
+        db_connection = db_connection_repository.find_by_id(
+            prompt_request.db_connection_id
+        )
+        if not db_connection:
+            raise DatabaseConnectionNotFoundError(
+                f"Database connection {prompt_request.db_connection_id} not found"
+            )
+
+        prompt = Prompt(
+            text=prompt_request.text,
+            db_connection_id=prompt_request.db_connection_id,
+            metadata=prompt_request.metadata,
+        )
+        return self.prompt_repository.insert(prompt)
