91ac7cc625705ddddf85139a3b4dc155d546a87d
Juan Valacco <97040903+jvalacco-dataherald@users.noreply.github.com>
2024-01-29 18:43:46 -0300

    DH-5333 self-serve sidebar reorganization

--- apps/ai/clients/admin-console/src/components/layout/sidebar-nav.tsx
+++ apps/ai/clients/admin-console/src/components/layout/sidebar-nav.tsx
@@ -14,8 +14,10 @@ import { Separator } from '@/components/ui/separator'
 import { useAppContext } from '@/contexts/app-context'
 import { cn } from '@/lib/utils'
 import {
+  BookOpenText,
   Building2,
   Database,
+  HelpCircle,
   KeyRound,
   LogOut,
   LucideIcon,
@@ -34,11 +36,11 @@ export interface MenuItem {
 }
 export type MenuItems = MenuItem[]
 
-const NAV_ITEMS: MenuItems = [
+const FIRST_NAV_ITEMS: MenuItems = [
   {
-    text: 'Queries',
-    href: '/queries',
-    icon: ShieldQuestion,
+    text: 'Playground',
+    href: '/playground',
+    icon: TerminalSquare,
   },
   {
     text: 'Databases',
@@ -46,22 +48,22 @@ const NAV_ITEMS: MenuItems = [
     icon: Database,
   },
   {
+    text: 'Queries',
+    href: '/queries',
+    icon: ShieldQuestion,
+  },
+  {
     text: 'Golden SQL',
     href: '/golden-sql',
     icon: ShieldCheck,
-  },
-]
-const CONFIG_NAV_ITEMS: MenuItems = [
-  {
-    text: 'Playground',
-    href: '/playground',
-    icon: TerminalSquare,
   },
   {
     text: 'Fine-tuning',
     href: '/fine-tuning',
     icon: SlidersIcon,
   },
+]
+const SECOND_NAV_ITEMS: MenuItems = [
   {
     text: 'API Keys',
     href: '/api-keys',
@@ -71,6 +73,19 @@ const CONFIG_NAV_ITEMS: MenuItems = [
     text: 'Organization',
     href: '/organization',
     icon: Building2,
+  },
+]
+
+const BOTTOM_NAV_ITEMS: MenuItems = [
+  {
+    text: 'Help',
+    href: 'https://www.dataherald.com/contact',
+    icon: HelpCircle,
+  },
+  {
+    text: 'Documentation',
+    href: 'https://docs.dataherald.com/',
+    icon: BookOpenText,
   },
 ]
 
@@ -95,7 +110,7 @@ const SidebarNav = ({
           className="w-full p-5"
         ></Image>
         <nav className={cn('flex flex-col', className)} {...props}>
-          {NAV_ITEMS.filter((i) => !i.hidden).map((item) => (
+          {FIRST_NAV_ITEMS.filter((i) => !i.hidden).map((item) => (
             <Link
               key={item.href}
               href={item.href}
@@ -118,7 +133,7 @@ const SidebarNav = ({
             </Link>
           ))}
           <Separator className="my-3" />
-          {CONFIG_NAV_ITEMS.filter((i) => !i.hidden).map((item) => (
+          {SECOND_NAV_ITEMS.filter((i) => !i.hidden).map((item) => (
             <Link
               key={item.href}
               href={item.href}
@@ -142,56 +157,83 @@ const SidebarNav = ({
           ))}
         </nav>
       </div>
-      {user && organization && (
-        <Popover>
-          <PopoverTrigger asChild>
-            <div className="flex flex-col items-center  gap-3 p-3 m-3 border rounded-xl bg-white cursor-pointer">
-              <div className="w-full flex items-center justify-between gap-2">
-                <div className="flex items-center gap-2">
-                  <UserPicture pictureUrl={user.picture} />
-                  <div className="flex flex-col">
-                    <span className="text-xs">{organization?.name}</span>{' '}
-                    <span className="font-semibold text-sm">{user.name}</span>
-                  </div>
-                </div>
-                <PopoverContent
-                  align="end"
-                  className="flex flex-col gap-3 ml-3 p-3"
-                >
-                  <div className="flex items-center justify-center gap-3">
-                    <div className="flex flex-col items-center gap-2 mt-1.5">
-                      <span className="text-xs text-slate-500">
-                        {user.email}
-                      </span>
-                      <span className="text-sm text-slate-900">
-                        {organization.name}
-                      </span>
+      <div className="flex flex-col">
+        {BOTTOM_NAV_ITEMS.filter((i) => !i.hidden).map((item) => (
+          <Link
+            key={item.href}
+            target="_blank"
+            rel="noopener noreferrer"
+            href={item.href}
+            className={cn(
+              buttonVariants({ variant: 'secondary-outline' }),
+              'bg-gray-50',
+              'hover:bg-gray-200',
+              'border-none',
+              'font-normal',
+              pathname?.includes(item.href)
+                ? 'bg-black/10 hover:bg-black/10 font-bold'
+                : '',
+              'justify-start',
+              'gap-2',
+              'px-5',
+            )}
+          >
+            <item.icon />
+            {item.text}
+          </Link>
+        ))}
+
+        {user && organization && (
+          <Popover>
+            <PopoverTrigger asChild>
+              <div className="flex flex-col items-center  gap-3 p-3 m-3 border rounded-xl bg-white cursor-pointer">
+                <div className="w-full flex items-center justify-between gap-2">
+                  <div className="flex items-center gap-2">
+                    <UserPicture pictureUrl={user.picture} />
+                    <div className="flex flex-col">
+                      <span className="text-xs">{organization?.name}</span>{' '}
+                      <span className="font-semibold text-sm">{user.name}</span>
                     </div>
                   </div>
-                  <div className="flex flex-col items-center justify-center gap-3 p-2">
-                    <h1>Hi, {user.name}!</h1>
-                    <UserPicture pictureUrl={user.picture} size={75} />
-                  </div>
-                  <Link href="/my-account">
-                    <Button variant="ghost" size="sm" className="w-full">
-                      <UserRound className="mr-2" size={18} /> My account
-                    </Button>
-                  </Link>
-                  <Separator />
-                  <Button
-                    variant="destructive-outline"
-                    size="sm"
-                    onClick={handleLogout}
+                  <PopoverContent
+                    align="end"
+                    className="flex flex-col gap-3 ml-3 p-3"
                   >
-                    <LogOut className="mr-2" size={18} />
-                    Sign out
-                  </Button>
-                </PopoverContent>
+                    <div className="flex items-center justify-center gap-3">
+                      <div className="flex flex-col items-center gap-2 mt-1.5">
+                        <span className="text-xs text-slate-500">
+                          {user.email}
+                        </span>
+                        <span className="text-sm text-slate-900">
+                          {organization.name}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="flex flex-col items-center justify-center gap-3 p-2">
+                      <h1>Hi, {user.name}!</h1>
+                      <UserPicture pictureUrl={user.picture} size={75} />
+                    </div>
+                    <Link href="/my-account">
+                      <Button variant="ghost" size="sm" className="w-full">
+                        <UserRound className="mr-2" size={18} /> My account
+                      </Button>
+                    </Link>
+                    <Separator />
+                    <Button
+                      variant="destructive-outline"
+                      size="sm"
+                      onClick={handleLogout}
+                    >
+                      <LogOut className="mr-2" size={18} />
+                      Sign out
+                    </Button>
+                  </PopoverContent>
+                </div>
               </div>
-            </div>
-          </PopoverTrigger>
-        </Popover>
-      )}
+            </PopoverTrigger>
+          </Popover>
+        )}
+      </div>
     </aside>
   )
 }
