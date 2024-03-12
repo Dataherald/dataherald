f097f6739ef4dc098df8641d6edeb4702926dc40
Juan Valacco <97040903+jvalacco-dataherald@users.noreply.github.com>
2024-03-12 16:51:32 -0300

    Ft/dh 5465 branch ephemeral environments (#475)
    * [DH-5515] Implement terraform for our backend services
    
    * Add git action to create and upload the images
    
    * Run terraform plan
    
    * Fix destroy ephemeral git action file
    
    * Replace special characters for the branch name
    
    * branch should be a lower case
    
    * Destroy if env creation fails
    
    * Check if the ECS is active
    
    * Add a timeout condition for destroy step
    
    * Fix condition to delete images
    
    * Update task definition
    
    * vercel-deploy
    
    * remove workflow dispatch
    
    * fix script
    
    * add missing token
    
    * exclude api test from running on other workflows changes
    
    * new attempt
    
    * decouple branch name from original branch
    
    * add custom build
    
    * remove build
    
    * remove env vars if already exists before creating them
    
    * continue on error
    
    * fix env var conflict
    
    * confirm removal
    
    * change deploy
    
    * deploy fixes
    
    * fix alias command
    
    * add vercel build + confirm
    
    * set root working directory
    
    * set root dir as workflow default
    
    * add pnpm
    
    * add permission to set alias domain
    
    * set domain scope command to be dataherald team -- set test api to akamai experiment ( to check if it's not using preview default and is using branch env vars
    
    * fix slack notification
    
    * prune special character on env vars
    
    * use printf to avoid new line issue
    
    * add new slack webhook
    
    * fix auth0 env var by adding protocol
    
    * remove prebuilt to take build output
    
    * pass env vars on deploy command
    
    * move to runtime env vars
    
    * renaming + remove var assignation on step outputs
    
    * deploy env vars time
    
    * general renaming structure - improve slack notification
    
    * fix http url
    
    * use secure protocol for back-end
    
    * improve slack failure notification
    
    * fix slack message newline
    
    * add cancelled notification
    
    * fix slack notification
    
    * DH-5521 added db iac
    
    * added env vars to github action
    
    * limit character for db name
    
    * Added dump file and github workflow for populating mongodb
    
    * DH-5521 make start self hosted runner independent
    
    * DH-5521 added parallel steps
    
    * DH-5521 added initial script for populating vetor db
    
    * DH-5521 added populate pinecone db script
    
    * removed mongo cluster creation
    
    * dump and restore staging on runner
    
    * merged frontend deployment
    
    * [DH-5575] Swith ports between core and server
    
    * Change ports
    
    * Fix API url
    
    * upgrade gh actions versions + branch env manual workflow + improve slack message
    
    * add details about manual workflow
    
    * temporary add current branch to test
    
    * trunc branch name characters
    
    * add resource restrictions to branch name
    
    * last changes
    
    ---------
    
    Co-authored-by: Juan Carlos Jose Camacho <jc@dataherald.com>
    Co-authored-by: dishenwang2023 <dishenwang1021@gmail.com>

---
+++ apps/ai/server/database/scripts/populate_pinecone_db/script.py
@@ -0,0 +1,55 @@
+import os
+
+import pymongo
+from langchain_openai import OpenAIEmbeddings
+from pinecone import Pinecone
+from sql_metadata import Parser
+
+pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+
+pinecone_index_name = os.environ.get("PINECONE_INDEX_NAME")
+mongodb_uri = os.environ.get("MONGODB_URI")
+mongodb_username = os.environ.get("MONGODB_USERNAME")
+mongodb_password = os.environ.get("MONGODB_PASSWORD")
+mongodb_name = os.environ.get("MONGODB_NAME", "ephemeral-serverless")
+
+openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+EMBEDDING_MODEL = "text-embedding-3-small"
+
+if __name__ == "__main__":
+    pinecone = Pinecone(api_key=pinecone_api_key)
+    mongodb_uri = mongodb_uri.replace("mongodb+srv://", "")
+    connection_string = (
+        f"mongodb+srv://{mongodb_username}:{mongodb_password}@{mongodb_uri}"
+    )
+    data_store = pymongo.MongoClient(connection_string)[mongodb_name]
+
+    golden_sqls = list(data_store["golden_sqls"].find())
+
+    embedding = OpenAIEmbeddings(openai_api_key=openai_api_key, model=EMBEDDING_MODEL)
+    index = pinecone.Index(pinecone_index_name)
+    batch_limit = 100
+    for limit_index in range(0, len(golden_sqls), batch_limit):
+        golden_sql_batch = golden_sqls[limit_index : limit_index + batch_limit]
+        embeds = embedding.embed_documents(
+            [record["prompt_text"] for record in golden_sql_batch]
+        )
+        records = []
+        for key in range(len(golden_sql_batch)):
+            parsed_tables = Parser(golden_sql_batch[key]["sql"]).tables
+            if len(parsed_tables) > 0:
+                records.append(
+                    (
+                        str(golden_sql_batch[key]["_id"]),
+                        embeds[key],
+                        {
+                            "tables_used": parsed_tables[0],
+                            "db_connection_id": golden_sql_batch[key][
+                                "db_connection_id"
+                            ],
+                        },
+                    )
+                )
+        index.upsert(vectors=records)
+    print("Indexing done!")
---
+++ apps/ai/server/terraform/main.tf
@@ -0,0 +1,271 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "aws"
+      version = "4.1.0"
+    }
+
+    pinecone = {
+      source  = "skyscrapr/pinecone"
+      version = "0.5.1"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-states2"
+    region = "us-east-1"
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+provider "pinecone" {}
+
+variable "branch_name" { type= string }
+
+variable "pinecone_index_name" {
+  description = "pinecone_index_name"
+  type        = string
+}
+
+variable "mongodb_uri" {
+  description = "mongodb_uri"
+  type        = string
+}
+
+variable "mongodb_name" {
+  description = "mongodb_name"
+  type        = string
+}
+
+variable "mongodb_username" {
+  description = "mongodb_username"
+  type        = string
+}
+
+variable "mongodb_password" {
+  description = "mongodb_password"
+  type        = string
+}
+
+variable "subnet_1_id" {
+  description = "subnet_1_id"
+  type        = string
+  default = "subnet-076afb4a159204349"
+}
+
+variable "subnet_2_id" {
+  description = "subnet_2_id"
+  type        = string
+  default = "subnet-0b6b9dbf631131b09"
+}
+
+variable "ecs_security_group_id" {
+  description = "security_group_id"
+  type        = string
+  default = "sg-07fac199a96aa3b65"
+}
+
+resource "pinecone_index" "my_index" {
+  name = var.pinecone_index_name
+  dimension = 1536
+  metric = "cosine"
+  spec = {
+    serverless = {
+      cloud = "aws"
+      region = "us-west-2"
+    }
+  }
+}
+
+locals {
+  srv_connection_string = replace(var.mongodb_uri, "mongodb+srv://", "mongodb+srv://${var.mongodb_username}:${var.mongodb_password}@")
+}
+
+resource "aws_ecs_task_definition" "my_task_definition" {
+  family = "k2-ephemeral-env-${var.branch_name}"
+  task_role_arn = "arn:aws:iam::422486916789:role/ecsk2TaskExecutionRole"
+  execution_role_arn = "arn:aws:iam::422486916789:role/ecsk2TaskExecutionRole"
+  network_mode = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu = "2048"
+  memory = "4096"
+#  container_definitions = file("task_definition.json")
+  container_definitions =<<DEFINITION
+[
+        {
+            "name": "k2-core-ephemeral-env",
+            "image": "422486916789.dkr.ecr.us-east-1.amazonaws.com/k2-core-ephemeral-envs:${var.branch_name}",
+            "cpu": 1024,
+            "memory": 3072,
+            "portMappings": [
+                {
+                    "name": "k2-core-ephemeral-env-80-tcp",
+                    "containerPort": 3001,
+                    "hostPort": 3001,
+                    "protocol": "tcp",
+                    "appProtocol": "http"
+                }
+            ],
+            "essential": true,
+            "environmentFiles": [
+                {
+                    "value": "arn:aws:s3:::ecs-k2-core-ephemeral-environment-variables/.env",
+                    "type": "s3"
+                }
+            ],
+            "environment": [
+              {
+                  "name": "CORE_PORT",
+                  "value": "3001"
+              },
+              {
+                "name": "MONGODB_DB_NAME",
+                "value": "${var.mongodb_name}"
+              },
+              {
+                "name": "MONGODB_URI",
+                "value": "${local.srv_connection_string}"
+              },
+              {
+                "name": "GOLDEN_SQL_COLLECTION",
+                "value": "${var.pinecone_index_name}"
+              }
+            ],
+            "command": ["sh", "-c", "uvicorn dataherald.app:app --host 0.0.0.0 --port $CORE_PORT --log-config log_config.yml --log-level debug --reload"],
+            "mountPoints": [],
+            "volumesFrom": [],
+            "logConfiguration": {
+                "logDriver": "awslogs",
+                "options": {
+                    "awslogs-create-group": "true",
+                    "awslogs-group": "/ecs/ecs-k2-ephemeral",
+                    "awslogs-region": "us-east-1",
+                    "awslogs-stream-prefix": "ecs"
+                }
+            }
+        },
+        {
+            "name": "k2-server-ephemeral-env",
+            "image": "422486916789.dkr.ecr.us-east-1.amazonaws.com/k2-server-ephemeral-envs:${var.branch_name}",
+            "cpu": 1024,
+            "memory": 3072,
+            "portMappings": [
+                {
+                    "name": "k2-server-ephemeral-env-3001-tcp",
+                    "containerPort": 80,
+                    "hostPort": 80,
+                    "protocol": "tcp",
+                    "appProtocol": "http"
+                }
+            ],
+            "essential": true,
+            "environmentFiles": [
+                {
+                    "value": "arn:aws:s3:::ecs-k2-server-ephemeral-environment-variables/.env",
+                    "type": "s3"
+                }
+            ],
+            "environment": [
+              {
+                  "name": "K2_CORE_URL",
+                  "value": "http://localhost:3001/api/v1"
+              },
+              {
+                "name": "MONGODB_DB_NAME",
+                "value": "${var.mongodb_name}"
+              },
+              {
+                "name": "MONGODB_URI",
+                "value": "${local.srv_connection_string}"
+              }
+            ],
+            "mountPoints": [],
+            "volumesFrom": [],
+            "logConfiguration": {
+                "logDriver": "awslogs",
+                "options": {
+                    "awslogs-create-group": "true",
+                    "awslogs-group": "/ecs/ecs-k2-ephemeral",
+                    "awslogs-region": "us-east-1",
+                    "awslogs-stream-prefix": "ecs"
+                }
+            }
+        }
+    ]
+  DEFINITION
+}
+
+resource "aws_lb" "my_load_balancer" {
+  name               = "${var.branch_name}"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["sg-07fac199a96aa3b65"]  # Replace with your security group ID
+  subnets            = ["subnet-076afb4a159204349", "subnet-0b6b9dbf631131b09"]  # Replace with your subnet IDs
+}
+
+resource "aws_lb_target_group" "ecs_target_group" {
+  name        = "${var.branch_name}"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = "vpc-09c492a49b76fdf80"
+  target_type = "ip"
+}
+
+resource "aws_lb_listener" "ecs_listener" {
+  load_balancer_arn = aws_lb.my_load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_target_group.arn
+  }
+}
+
+resource "aws_lb_listener" "https_listener" {
+  load_balancer_arn = aws_lb.my_load_balancer.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"  # Choose an appropriate SSL policy for your application
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_target_group.arn
+  }
+  certificate_arn = "arn:aws:acm:us-east-1:422486916789:certificate/0159c510-ecbb-4607-bb4e-0df6be7b44ab"
+}
+
+resource "aws_ecs_service" "my_service" {
+  name            = "k2-ephemeral-env-${var.branch_name}"
+  cluster         = "arn:aws:ecs:us-east-1:422486916789:cluster/k2"
+  task_definition = aws_ecs_task_definition.my_task_definition.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  enable_ecs_managed_tags = true
+  wait_for_steady_state   = true
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.ecs_target_group.arn
+    container_name   = "k2-server-ephemeral-env"
+    container_port   = 80
+  }
+
+  network_configuration {
+    subnets         = [var.subnet_1_id, var.subnet_2_id]
+    security_groups = [var.ecs_security_group_id]
+    assign_public_ip = true
+  }
+}
+
+resource "aws_route53_record" "my_load_balancer_record" {
+  zone_id = "Z07539241TW7P7NHVR11T"  # Replace with your Route 53 hosted zone ID
+  name    = "${var.branch_name}.api.dataherald.ai"   # Replace with the desired domain name
+  type    = "A"
+  alias {
+    name                   = aws_lb.my_load_balancer.dns_name
+    zone_id                = aws_lb.my_load_balancer.zone_id
+    evaluate_target_health = true
+  }
+}
