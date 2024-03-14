cb582168763ed8877a96705dc73d46943a50cb72
Juan Carlos José Camacho <jc@dataherald.com>
2024-03-14 12:13:24 -0600

    [DH-5601] Add health check for load balancers (#480)

--- apps/ai/server/terraform/main.tf
+++ apps/ai/server/terraform/main.tf
@@ -211,6 +211,15 @@ resource "aws_lb_target_group" "ecs_target_group" {
   protocol    = "HTTP"
   vpc_id      = "vpc-09c492a49b76fdf80"
   target_type = "ip"
+
+  health_check {
+    path                = "/heartbeat"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    matcher             = "200-299"
+  }
 }
 
 resource "aws_lb_listener" "ecs_listener" {
